f112ae557317da7820994263f52bb61cf4e2b8b1
Ahmed Hamza El-Sherif <119296268+0xelsherif@users.noreply.github.com>
2024-05-02 05:08:19 +0300

    copyright

--- footer.js
+++ footer.js
@@ -1,7 +1,8 @@
-$('.flyout-trigger').click((e) => {
-    let pressed = $(e.target).attr('aria-pressed') === 'true';
-    
-    $(e.target).attr('aria-pressed', `${!pressed}`);
-    
-    $('.flyout').toggleClass('open');
-  });
+document.getElementById("toggle-copyright-form").addEventListener("click", function() {
+  var copyrightForm = document.querySelector(".copyright-form");
+  if (window.getComputedStyle(copyrightForm).display === "none") {
+      copyrightForm.style.display = "block";
+  } else {
+      copyrightForm.style.display = "none";
+  }
+});
